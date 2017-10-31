00f05413e884fb2639404a9436d37baa19532b63
Viet Trung <tranviettrung92@gmail.com>
2017-10-31 09:58:21 +0700

    Init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tranv on 30/10/2017.
+ */
+
+$(document).ready(function() {
+    $('.carousel').carousel();
+
+    var countdownClock = $('#clock');
+
+    var countdownTime = countdownClock.data('countdown-time');
+    countdownClock.countdown(countdownTime, function(event) {
+        $(this).html(event.strftime('<span class="day">%D</span> : <span class="hour">%H</span> : <span class="minute">%M</span> : <span class="second">%S</span>'));
+    });
+});
