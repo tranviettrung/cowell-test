ea32c8e74f1e1373e786ce0f9c1ca6d7c24b37db
Viet Trung <tranviettrung92@gmail.com>
2017-10-31 14:16:39 +0700

    add ajax call

--- js/script.js
+++ js/script.js
@@ -11,4 +11,22 @@ $(document).ready(function() {
     countdownClock.countdown(countdownTime, function(event) {
         $(this).html(event.strftime('<span class="day">%D</span> : <span class="hour">%H</span> : <span class="minute">%M</span> : <span class="second">%S</span>'));
     });
+
+    $('#register-form').on('submit', function(event) {
+        event.preventDefault();
+        var email = $('#register-email').val();
+
+        $.ajax({
+            type: "POST",
+            url: 'http://59e57ad3f99ad90012268277.mockapi.io/event',
+            data: {
+                email: email
+            },
+            success: function (response) {
+                console.log(response);
+                $('#register-email').val('');
+                alert('Register successfully');
+            }
+        });
+    });
 });
